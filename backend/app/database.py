e47ccd53e53e09980bf22ec0a9af9f9f0c204e43
Soham <sohamrw098@gmail.com>
2025-09-14 11:14:15 +0000

    5.4 config(db): define SQLAlchemy engine and session for Alembic integration

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.config import settings
+from backend.app.config import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
