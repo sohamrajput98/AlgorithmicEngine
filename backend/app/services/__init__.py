594dff4ddcca45ec7bd819f253a2068633a18842
Soham <sohamrw098@gmail.com>
2025-09-18 14:45:02 +0000

    13.3: Clean up service init to avoid circular import

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -3,6 +3,6 @@ from .user_service import UserService
 from .problem_service import ProblemService
 from .testcase_service import TestCaseService
 from .submission_service import SubmissionService
-from .badge_service import BadgeService
+#from .badge_service import BadgeService
 from .analytics_service import AnalyticsService
 from .feature_flag_service import FeatureFlagService
